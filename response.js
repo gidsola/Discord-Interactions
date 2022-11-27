a893fc176ce143306e4a10d7eb098ac2b67c4c62
gidsola <mgoodie@outlook.com>
2022-11-27 12:40:06 -0500

    remove processed return. edit_original originally returned: return JSON.parse(r.body.toString()); Now only returns r; The same is for get_original

--- response.js
+++ response.js
@@ -130,7 +130,7 @@ module.exports.callback = {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ type: 8, data: { input } }),
       }); return r;
-    } catch (e) { return e; }
+    } catch (e) { return e }
   },
 
   /**
@@ -170,8 +170,8 @@ module.exports.callback = {
         path: encodeURI(`/api/webhooks/${interaction.application_id}/${interaction.token}/messages/@original`),
         headers: { 'Content-Type': 'application/json' },
         body: '',
-      }); return JSON.parse(r.body.toString());
-    } catch (e) { return e; }
+      }); return r;
+    } catch (e) { return e }
   },
 
   /**
@@ -195,7 +195,7 @@ module.exports.callback = {
           path: encodeURI(url),
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(input),
-        }); return r ? JSON.parse(r.body.toString()) : a;
+        }); return r ? r : a;
     } catch (e) { return e }
   },
 
@@ -242,7 +242,7 @@ module.exports.followup = {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(input),
         }); return r ? r : a;
-    } catch (e) { return e; }
+    } catch (e) { return e }
   },
 
   /**
